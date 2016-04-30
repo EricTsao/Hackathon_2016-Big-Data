1a5510774ffb37e2cab8d7115642069cfc455d1e
LinSianTing <SianTingTaiwan@gmail.com>
2016-04-30 15:57:26 +0800

    第二支 機器人
    第二支 機器人

---
+++ Robot/Robot_GetNumberCount/Robot_GetNumberCount_ClassLibrary_V1.0.0/Robot/Robot.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot.Robot_ClassLibrary
+{
+    public partial class Robot
+    {
+        public string Access_Token { get; set; }
+
+        /// <summary>
+        /// 一般初始化
+        /// </summary>
+        public Robot()
+        {
+            Access_Token = "";
+        }
+
+        /// <summary>
+        /// 指定預設初始化
+        /// </summary>
+        public Robot(string access_Token)
+        {
+            //Access_Token
+            Access_Token = access_Token;
+        }
+    }
+}
